047a99851914087f3646744bf1ec390601c2a3ef
natradaya <natrada.yaw@dome.tu.ac.th>
2025-11-15 18:55:50 +0700

    update 15/11

--- player.py
+++ player.py
@@ -3,15 +3,13 @@ from pathlib import Path
 import setting as s
 
 pygame.init()
-#attck_sound = pygame.mixer.Sound("game/image/assets/attack_sf.wav")
-#attck_sound.play()
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y,):
+    def __init__(self, pos, group,):
         super().__init__()
         # กำหนดขนาดมาตรฐาน
-        self.frame_size = (130, 130)
-        self.attack_size = (150,150)
+        self.frame_size = (160, 160)
+        self.attack_size = (180,180)
 
         self.animations = {
             'down': [],  
@@ -46,9 +44,9 @@ class Player(pygame.sprite.Sprite):
 
         self.current_sprite = 0
         self.current_direction = 'down'
-        self.anim_speed = 0.17
+        self.anim_speed = 0.2
         self.image = self.animations[self.current_direction][0]
-        self.rect = self.image.get_rect(topleft=(pos_x, pos_y))
+        self.rect = self.image.get_rect(center=pos)
 
     def update(self):
         keys = pygame.key.get_pressed()
@@ -108,8 +106,60 @@ class Player(pygame.sprite.Sprite):
         self.image = frames[int(self.current_sprite)]
         self.rect = self.image.get_rect(topleft=old_topleft)
 
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+        # camera offset 
+        self.offset = pygame.math.Vector2()
+        self.half_w = self.display_surface.get_size()[0] //2
+        self.half_h = self.display_surface.get_size()[1] //2
+          
+        # box setup
+        self.camera_borders = {'left': 100, 'right': 100, 'top': 100, 'bottom': 100}
+        l = self.camera_borders['left']
+        t = self.camera_borders['top']
+        w = self.display_surface.get_size()[0]  - (self.camera_borders['left'] + self.camera_borders['right'])
+        h = self.display_surface.get_size()[1]  - (self.camera_borders['top'] + self.camera_borders['bottom'])
+        self.camera_rect = pygame.Rect(l,t,w,h)
+          
+        #ground
+        ground = pygame.image.load("game/image/background/floor.png").convert_alpha()
+        self.ground_surf = pygame.transform.scale(ground,(1600,1200))
+        self.ground_rect = self.ground_surf.get_rect(topleft=(0,0))
+          
+    def center_target_camera(self,target):
+        self.offset.x = target.rect.centerx - self.half_w
+        self.offset.y = target.rect.centery - self.half_h
+    
+    def box_target_camera(self,target):
+        if target.rect.left < self.camera_rect.left:
+            self.camera_rect.left = target.rect.left
+        if target.rect.right > self.camera_rect.right:
+            self.camera_rect.right = target.rect.right
+        if target.rect.top < self.camera_rect.top:
+            self.camera_rect.top = target.rect.top
+        if target.rect.bottom > self.camera_rect.bottom:
+            self.camera_rect.bottom = target.rect.bottom
+
+        self.offset.x = self.camera_rect.left - self.camera_borders['left']
+        self.offset.y = self.camera_rect.top - self.camera_borders['top']
+        
+    def custom_draw(self,player):
+        self.center_target_camera(player)
+
+        #ground
+        ground_offset = self.ground_rect.topleft - self.offset
+        self.display_surface.blit(self.ground_surf,ground_offset)
+
+        pygame.draw.rect(self.display_surface,'yellow',self.camera_rect,5)
+        
+            
+#camera setup
+camera_group = CameraGroup()
+
 # Player setup
-player = Player(s.center_y-25, s.center_x+250,)  
+player = Player((750,500),camera_group)
 moving_sprites = pygame.sprite.Group()
 moving_sprites.add(player)
-
